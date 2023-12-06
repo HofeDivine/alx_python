1edb1e7ab300bf0b2c825b5aa06206a3a8d29c18
HofeDivine <hofedive@gmail.com>
2023-12-05 22:18:25 -1200

    commit1

---
+++ python-hello_world/1-edges.py
@@ -0,0 +1,8 @@
+word = "Holberton"
+word_first_3 = word[:3]
+print("First 3 letters: {}".format(word_first_3))
+word_last_2 = word[-2:]
+print("Last 2 letters: {}".format(word_last_2))
+middle_index = len(word) // 2
+middle_word = word[middle_index - 1:middle_index + 2]
+print("Middle word: {}".format(middle_word))
