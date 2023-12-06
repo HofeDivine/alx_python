d18257e8221197cf5fb20744d7eee0f11d359552
HofeDivine <hofedive@gmail.com>
2023-12-05 22:24:56 -1200

    comm1

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -3,6 +3,5 @@ word_first_3 = word[:3]
 print("First 3 letters: {}".format(word_first_3))
 word_last_2 = word[-2:]
 print("Last 2 letters: {}".format(word_last_2))
-middle_index = len(word) // 2
-middle_word = word[middle_index - 1:middle_index + 2]
+middle_word = word[1:-1]
 print("Middle word: {}".format(middle_word))
