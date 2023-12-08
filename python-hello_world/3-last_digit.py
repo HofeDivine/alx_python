3d90b7037468dc73fa136aba6edbcfbe25ced3e1
HofeDivine <hofedive@gmail.com>
2023-12-07 18:52:44 -1200

    commit333

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -9,5 +9,8 @@ elif Lastdigit == 0:
     result = "{}  {} is {} and {}".format("last digit of",number,Lastdigit, " is 0"  )
     print(result)
 elif Lastdigit < 5:
-     result = "{}  {} is {} and {}".format("last digit of",number,Lastdigit, "isless than 5"  )
+     result = "{}  {} is {} and {}".format("last digit of",number,Lastdigit, "is less than 5"  )
      print(result)
+elif Lastdigit < 6 and not 0:
+    result = "{}  {} is {} and {}".format("last digit of",number,Lastdigit, "and is less than 6 and not 0"  )
+    print(result)
