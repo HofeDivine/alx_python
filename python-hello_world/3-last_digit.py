173ee08e50d70afbeb78c9a835a1c7cf2a3ceba6
HofeDivine <hofedive@gmail.com>
2023-12-07 18:46:43 -1200

    com33

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -3,11 +3,11 @@ import random
 number = random.randint(-10000, 10000)
 Lastdigit = int(str(number)[-1])
 if Lastdigit > 5:
-    result = "{} {} is {} and {}".format("Last digit of",number,Lastdigit, "greater than 5"  )
+    result = "{} {} is {} and {}".format("Last digit of",number,Lastdigit, "is greater than 5"  )
     print(result)
 elif Lastdigit == 0:
     result = "{}  {} is {} and {}".format("last digit of",number,Lastdigit, " is 0"  )
     print(result)
 elif Lastdigit < 5:
-     result = "{}  {} is {} and {}".format("last digit of",number,Lastdigit, "less than 5"  )
+     result = "{}  {} is {} and {}".format("last digit of",number,Lastdigit, "isless than 5"  )
      print(result)
