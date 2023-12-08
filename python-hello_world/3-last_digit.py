ca149698dcbf629ff4075f14aa8e774acc672d08
HofeDivine <hofedive@gmail.com>
2023-12-07 19:24:15 -1200

    commit3f

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -6,11 +6,12 @@ if Lastdigit > 5:
     result = "{} {} is {} and {}".format("Last digit of",number,Lastdigit, "is greater than 5"  )
     print(result)
 elif Lastdigit == 0:
-    result = "{}  {} is {} and {}".format("last digit of",number,Lastdigit, " is 0"  )
+    result = "{}  {} is {} and {}".format("last digit of",number,Lastdigit, "is 0"  )
     print(result)
-elif Lastdigit < 5:
-     result = "{}  {} is {} and {}".format("last digit of",number,Lastdigit, "is less than 5"  )
-     print(result)
-elif Lastdigit < 6 and not 0:
-    result = "{}  {} is {} and {}".format("last digit of",number,Lastdigit, "and is less than 6 and not 0"  )
+elif Lastdigit < 6 and not 0 and number >0:
+    result = "{}  {} is {} and {}".format("last digit of",number,Lastdigit, "is less than 6 and not 0"  )
+    print(result)
+if  number < 0 and Lastdigit !=0:
+    Lastdigit = Lastdigit *-1
+    result = "{}  {} is {} and {}".format("last digit of",number,Lastdigit, "is less than 6 and not 0"  )
     print(result)
