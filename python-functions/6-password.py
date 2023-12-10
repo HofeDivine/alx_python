3e27f743eef5dd2245e551291707c1f0dabc2f07
HofeDivine <hofedive@gmail.com>
2023-12-09 19:15:03 -1200

    comm6

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -1,7 +1,7 @@
 def validate_password(password):
     if any(char.isspace() for char in password):
        return False
-    elif len(password) >=8 and any(char.isupper() for char in password ) and any(char.islower() for char in password):
+    elif len(password) >=8 and any(char.isupper() for char in password ) and any(char.islower() for char in password) and any(char.isdigit() for char in password):
         return True
     else:
         return False
