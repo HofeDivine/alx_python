13009d7f0fcacc723517dc4c39ce2e96b815c77e
HofeDivine <hofedive@gmail.com>
2023-12-12 15:28:19 -1200

    00

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,10 @@
-
-import add_0
 a=1
 b=2
+import add_0
+
 if __name__ == "__main__":
  
  def add(a,b):
  
   return a+b
-print("{} + {} = {}".format(1,2, add(a,b)))
+print("{} + {} = {}".format(a,b, add(a,b)))
