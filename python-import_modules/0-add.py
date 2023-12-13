022e85fff0dbc1fb4956cddf5c1fef3f14445792
HofeDivine <hofedive@gmail.com>
2023-12-12 15:52:27 -1200

    00

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,5 +1,5 @@
-a=1
-b=2
+a = 1
+b = 2
 
 import add_0
 
--- python-import_modules/add_0.py
+++ python-import_modules/add_0.py
@@ -1,6 +1,6 @@
 def add(a,b):
-    a=1
-    b=2
+    a = 1
+    b = 2
     return (a+b)
     print("{} + {} = {}".format(1, 2, add(1, 2)))
     
