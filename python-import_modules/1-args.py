d3883992976bea6493ae48978884314c1d02df2c
HofeDivine <hofedive@gmail.com>
2023-12-13 11:50:35 -1200

    commit1

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -3,13 +3,13 @@ from sys import argv
 lengthy = len(argv)-1
 if __name__ == "__main__":
     if lengthy == 0:
-        print("{} arguments".format(lengthy))
+        print("{} arguments.".format(lengthy))
     elif lengthy == 1:
-      print("{} argument".format(lengthy))
+      print("{} argument:".format(lengthy))
       print("{}: {} ".format(lengthy,argv[1] ))
     else :
-       print("{} arguments".format(lengthy))
+       print("{} arguments:".format(lengthy))
        for i in range(lengthy):
-         print("{}: {} ".format(i+1,argv[i] ))
+         print("{}: {} ".format(i+1,argv[i+1] ))
     
        
