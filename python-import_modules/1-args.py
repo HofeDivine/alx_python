c58b81f457a69ee79f08b5ed3d6116eb321c9d58
HofeDivine <hofedive@gmail.com>
2023-12-13 11:42:10 -1200

    commit1

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,14 +1,15 @@
-listOfItems = ["Hello","Holberton","School",98,"Battery","street"]
-lengthy = len(listOfItems)
+from sys import argv
+
+lengthy = len(argv)-1
 if __name__ == "__main__":
     if lengthy == 0:
         print("{} arguments".format(lengthy))
     elif lengthy == 1:
       print("{} argument".format(lengthy))
-      print("{}: {} ".format(lengthy,listOfItems[1] ))
+      print("{}: {} ".format(lengthy,argv[1] ))
     else :
        print("{} arguments".format(lengthy))
        for i in range(lengthy):
-         print("{}: {} ".format(i+1,listOfItems[i] ))
+         print("{}: {} ".format(i+1,argv[i] ))
     
        
