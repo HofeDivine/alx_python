8e82eac519c1f777dc467f1fc7ae3779e76f69fa
HofeDivine <hofedive@gmail.com>
2023-12-13 13:52:12 -1200

    33

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -2,6 +2,7 @@ def safe_print_division(a, b):
     try:
         result = a/b
     except ZeroDivisionError:
+        print("Inside result: {}".format("None"))
         return None
     else:
         print("Inside result: {}".format(result))
