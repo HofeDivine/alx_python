847b83bcb6766ec1ddc84b92a33a965b5a05e324
HofeDivine <hofedive@gmail.com>
2023-12-13 17:02:06 -1200

    commit1

---
+++ python-data_structures/1-print_matrix_integer.py
@@ -0,0 +1,9 @@
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i, value in enumerate(row):
+            if i == len(row) - 1:
+                print("{:d}".format(value), end="")
+            else:
+                print("{:d} ".format(value), end="")
+        print()  
+
