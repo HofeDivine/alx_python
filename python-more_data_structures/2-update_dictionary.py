2362c5d9c5d7ae3ae57d738d051f84635a5a3069
HofeDivine <hofedive@gmail.com>
2023-12-14 15:47:23 -1200

    commit2

---
+++ python-more_data_structures/2-update_dictionary.py
@@ -0,0 +1,7 @@
+def update_dictionary(a_dictionary, key, value):
+    my_dict = a_dictionary
+    if key in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        a_dictionary[key] = value
+    return my_dict
