af184a54ef1760ecbb3707c87bc0d7e473935464
HofeDivine <hofedive@gmail.com>
2023-12-16 12:39:19 -1200

    commit0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -4,5 +4,11 @@ class Square:
     :param size is the size of the square
     """
     def __init__(self,size):
-        self.__size = size
+        
+         """
+        Initializes a new Square instance with the given size.
+
+        :param size: The size of the square.
+        """
+         self.__size = size
         
