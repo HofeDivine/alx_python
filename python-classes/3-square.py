7cfc55ba7e1423232b30ecab4002186bf18dae4b
HofeDivine <hofedive@gmail.com>
2023-12-16 15:42:14 -1200

    commit3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -15,20 +15,22 @@ class Square:
     def area(self):
         square_area = self.__size**2
         return square_area
-    def get_size(self):
+    def size(self):
         """
-        a function to get the private instance size
+        A getter property to get the private instance size
         :return: The size of the square.
         """
         return self.__size
-    def set_size(self,size):
+
+    
+    def size(self, value):
         """
-        a method that allows us to set a new size of our private instance of the class square
-        :param size: The new size to set.
+        A setter property that allows us to set a new size of our private instance of the class square
+        :param value: The new size to set.
         """
-        if not isinstance(size,int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
     
