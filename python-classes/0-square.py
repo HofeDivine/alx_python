56ba8588a2adf169132f16555b2da1e7553c7e0a
HofeDivine <hofedive@gmail.com>
2023-12-16 12:49:24 -1200

    commit 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -11,4 +11,15 @@ class Square:
         :param size: The size of the square.
         """
          self.__size = size
+    def get__size(self,size):
+         """
+         a function to get the private instance size
+         """
+         return self.__size
+    def set__size(self,size):
+         """
+         a metod that allows us to set a new size of our private instance of the class square
+         """
+         return self.__size
+
         
