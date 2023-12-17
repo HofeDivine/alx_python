45e345fbd334050773b63eddda8970ead16996da
HofeDivine <hofedive@gmail.com>
2023-12-16 13:11:03 -1200

    com0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -5,23 +5,23 @@ class Square:
     """
     def __init__(self,size):
         
-         """
+        """
         Initializes a new Square instance with the given size.
 
         :param size: The size of the square.
         """
-         self.__size = size
+        self.__size = size
     def get_size(self):
-         """
-         a function to get the private instance size
-         :return: The size of the square.
-         """
-         return self.__size
+        """
+        a function to get the private instance size
+        :return: The size of the square.
+        """
+        return self.__size
     def set_size(self,size):
-         """
-         a metod that allows us to set a new size of our private instance of the class square
-         :param size: The new size to set.
-         """
-         self.__size = size
+        """
+        a method that allows us to set a new size of our private instance of the class square
+        :param size: The new size to set.
+        """
+        self.__size = size
 
         
