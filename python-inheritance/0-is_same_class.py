c67691d462b98b026e85ef470cae9483b69fec9c
HofeDivine <hofedive@gmail.com>
2023-12-16 19:11:26 -1200

    commit0

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,3 +1,6 @@
+"""
+This function helps chect thhe type of an object to see if it match corresponding class
+"""
 def is_same_class(obj, a_class):
     """
     this fuction checks if an object is an instance of a certain class
