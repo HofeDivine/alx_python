d8f9b50474ec47012e4c7004e6f4c8ad8e544059
HofeDivine <hofedive@gmail.com>
2023-12-18 12:41:03 -1200

    commit444

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -6,6 +6,9 @@ class BaseGeometry:
     Empty class
     """
     def area(self):
+        """
+        Raises an Exception with the message "area() is not implemented".
+        """
         raise Exception("area() is not implemented")
-        return False
+    
     
