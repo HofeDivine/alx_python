819c768451d668bb490a3a4bdd1e59ed27215bdc
HofeDivine <hofedive@gmail.com>
2023-12-18 15:07:44 -1200

    commi4

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,11 +1,11 @@
-"""
-Empty class
-"""
 class BaseGeometry:
     """
-    Empty class
+    BaseGeometry class with a public instance method area().
     """
+
     def area(self):
-        raise Exception("{}".format("area() is not implemented"))
-    
-    
+        """
+        Public instance method that raises an Exception with the message
+        "area() is not implemented".
+        """
+        raise Exception("area() is not implemented")
