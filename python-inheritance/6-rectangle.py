3f0382ee06144283ac89d6f3fd377792053134d1
HofeDivine <hofedive@gmail.com>
2023-12-20 11:36:01 -1200

    commit6

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,8 +1,21 @@
-class BaseGeometry:
+
+"""
+BaseGeometry class with a public instance method area().
+"""
+class MetaClass(type):
+    """
+    Oeveride dir() method to exclude __init__subclass__
+    """
+    def __dir__(cls):
+        return[attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
+
+class BaseGeometry(metaclass=MetaClass):
     """
     BaseGeometry class with a public instance method area().
     """
-
+    def __dir__(cls):
+        return[attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
     def area(self):
         """
         Public instance method that raises an Exception with the message
