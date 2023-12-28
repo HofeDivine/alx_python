50ec993b71990e2cb7e8f902278af91461d6c72d
HofeDivine <hofedive@gmail.com>
2023-12-27 17:23:17 -1200

    commit

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,26 +1,7 @@
 """
 we are diving deep into python. This project is about python alnost a circle
 """
-class Base:
-    """
-    this is base class haiving a private attributes __nb_objects
-    """
-    __nb_objects = 0
-    def __init__(self, id=None):
-        """
-        this in the construstor of the base class
-        it has id as an instance attributes
-        """
-        self.id = id
-        if id is not None:
-           self.id = id 
-        else:
-            Base.__nb_objects +=1
-            self.id = Base.__nb_objects
-
-"""
-this module contains a rectangle class with private instace attributes
-"""
+from models.base import Base
 class Rectangle(Base):
     """
     this is a rectangle class that contains the private attributes
