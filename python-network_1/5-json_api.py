4a6857142e84bb487b662f3ff2b732a1c6ad65c6
HofeDivine <hofedive@gmail.com>
2024-01-13 17:08:04 -1200

    comit

---
+++ python-network_1/5-json_api.py
@@ -0,0 +1,25 @@
+"""
+ Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.
+ """
+import requests
+import sys
+def post_email(q):
+    url = "http://0.0.0.0:5000/search_user"
+    response = requests.post(url,q)
+    if response.headers == response.json():
+        user_id = response.json.get('id')
+        user_name = response.json.get('name')
+        print("[{}] {}".format(user_id,user_name))
+    elif not response.json:
+        print("No result")
+    else:
+        print("Not a valid JSON")
+
+if __name__ =="__main__":
+    if len(sys.argv)!= 2:
+        sys.exit(1)
+    elif len(sys.argv)== 1:
+         q=""
+    else:
+        q = sys.argv[1]
+    post_email(q)
