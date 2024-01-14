610a7975e8edb3dd37d8380042affb77979dfc81
HofeDivine <hofedive@gmail.com>
2024-01-13 17:51:33 -1200

    commit

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -1,27 +1,35 @@
 """
  Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.
  """
+"""
+Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
 import requests
 import sys
+
 def post_email(q):
     url = "http://0.0.0.0:5000/search_user"
-    data = {'q':q}
-    response = requests.post(url,data)
-    if response.headers['content-type'] =='application/json' and response.json():
-        user_data = response.json()
-        user_id = user_data.get('id')
-        user_name = user_data.get('name')
-        print("[{}] {}".format(user_id,user_name))
-    elif not response.json():
-         print("No result")
-    else:
+    data = {'q': q}
+    
+    try:
+        response = requests.post(url, data=data)
+        response_json = response.json()
+
+        if response.headers['content-type'] == 'application/json' and response_json:
+            user_id = response_json.get('id')
+            user_name = response_json.get('name')
+            print("[{}] {}".format(user_id, user_name))
+        elif not response_json:
+            print("No result")
+        else:
+            print("Not a valid JSON")
+
+    except requests.exceptions.JSONDecodeError:
         print("Not a valid JSON")
 
-if __name__ =="__main__":
-    if len(sys.argv)!= 2:
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
         sys.exit(1)
-    elif len(sys.argv)== 1:
-         q=""
-    else:
-        q = sys.argv[1]
+
+    q = sys.argv[1]
     post_email(q)
