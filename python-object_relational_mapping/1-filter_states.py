10c53e8112229ad6572487c27e439177d060d0c9
HofeDivine <hofedive@gmail.com>
2024-01-29 08:38:04 -1200

    commit

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,28 @@
+"""
+A script that list the name of states that starts with N in a certain database 
+Its takes username ,password, database as arguments
+"""
+import MySQLdb
+import sys
+
+def list_N_states(username,password,database):
+    """
+    A function that list the states starting with N
+    It uses .connect() method of MYSQLdb module to connect python script to mysql
+    """
+    myconnect = MySQLdb.connect(host = "localhost",port =3306,user =username,passwd =password,database =database)
+    cursor1 = myconnect.cursor()
+    cursor1.execute("SELECT * FROM states LIKE 'N%' ORDER BY id ASC")
+    myrows = cursor1.fetchall
+    for rows in myrows:
+        print(myrows)
+    cursor1.close
+    myconnect.close
+
+if __name__ == "main":
+        if len(sys.argv) != 4:
+            sys.exit(1)
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database = sys.argv[3]
+        list_N_states(username,password,database)
