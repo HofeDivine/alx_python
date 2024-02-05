ea2e880cb1ba8c9155e7fe0d7d0be8a07ab67bb3
HofeDivine <hofedive@gmail.com>
2024-02-05 10:38:34 -1200

    commit

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,30 @@
+"""
+a script that lists all State objects from the database hbtn_0e_6_usa
+
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orn import sessionmaker
+from model_state import Base,State
+def all_states(username,password,database):
+    """
+    a function that lists all State objects from the database hbtn_0e_6_usa
+
+    """
+    path = "mysql://username:password@localhost:3306/database"
+    engine = create_engine(path)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    mystates =session.query(State).order_by(State.id).all()
+    for state in mystates:
+        print(f"{state.id}: {state.name}")
+if __name__ == "__main__":
+    if len(sys.argv)!= 4:
+        sys.exit(1)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    all_states(username,password,database)
+
+
+
