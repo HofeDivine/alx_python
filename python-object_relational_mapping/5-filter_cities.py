f048cdf4cbe891bad5985d600c1a0fb21d5f5ddd
HofeDivine <hofedive@gmail.com>
2024-02-04 18:23:27 -1200

    commit

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,35 @@
+"""a script that takes in the name of a state
+ as an argument and lists all cities of that state, using the database hbtn_0e_4_usa"""
+import MySQLdb
+import sys
+def all_cities(username,password,database,state_name):
+    """
+    a function that takes in the name of a state as an argument and lists all cities of that state, 
+    using the database hbtn_0e_4_usa
+    """
+    connection =MySQLdb.connect(host="localhost",port = 3306,user=username,passwd =password,db=database)
+    cursor =connection.cursor()
+    querry=     """
+                SELECT cities.name,cities.id,states.name
+                FROM cities
+                JOIN states ON cities.id = states.id
+                WHERE states.name = %s
+                ORDER BY Cities.id ASC
+
+                """
+    cursor.execute(querry,(state_name,))
+    myrows = cursor.fetchall()
+    for rows in myrows:
+        print(rows)
+    cursor.close()
+    connection.close()
+if __name__ == "__main__":
+    if len(sys.argv)!=5:
+        sys.exit(1)
+    username = sys.argv[1]
+    password= sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+    all_cities(username,password,database,state_name)    
+    
+    
