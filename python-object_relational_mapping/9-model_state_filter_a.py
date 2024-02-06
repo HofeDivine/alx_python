56e160e7e1b658ba9631ecddedcd8c074102cf41
HofeDivine <hofedive@gmail.com>
2024-02-05 12:55:12 -1200

    commit

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -15,7 +15,7 @@ def a_states(username,password,database):
     engine = create_engine(path)
     Session = sessionmaker(bind=engine)
     session = Session()
-    a_states = session.query(State).filterI(State.name.like('%a%')).order_by(State.id).all()
+    a_states = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
     for state in a_states:
         print(f"{state.id}: {state.name}")
 
