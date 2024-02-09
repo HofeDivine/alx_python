abe5e851d43ae590f015ecfe38c86f4ebfba7753
HofeDivine <hofedive@gmail.com>
2024-02-09 08:32:43 -1200

    commit

---
+++ python-web_framework/3-python_route.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route('/',strict_slashes=False)
+def hello():
+    return 'Hello HBNB'
+@app.route('/hbnb')
+def Hhello():
+    return 'HBNB'
+@app.route('/c/<text>')
+def displayC(text):
+     return 'C {}'.format(text.replace('_', ' '))
+@app.route('/python/<text>')
+def displayPy(text="is cool"):
+    return 'Python {}'.format(text.replace('_', ' '))
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0',port=5000)
