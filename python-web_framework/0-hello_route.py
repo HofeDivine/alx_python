f46a35f2df5177298d61a361158cc76fe9397ed9
HofeDivine <hofedive@gmail.com>
2024-02-08 20:31:39 -1200

    commit

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,7 +1,14 @@
+"""
+A script   that starts a Flask web application:
+listening on host 0.0.0.0 and port 5000
+"""
 from flask import Flask
 app = Flask(__name__)
 @app.route('/',strict_slashes=False)
 def hello():
+    """
+    A function that returns a string on the homepage of a web application
+    """
     return "Hello HBNB!"
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000)
