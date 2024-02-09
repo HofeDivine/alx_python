8e2892c4a2b479a4c67f98c818bfbe84d88c527d
HofeDivine <hofedive@gmail.com>
2024-02-09 09:04:45 -1200

    comit

--- python-web_framework/3-python_route.py
+++ python-web_framework/3-python_route.py
@@ -9,14 +9,14 @@ app = Flask(__name__)
 @app.route('/',strict_slashes=False)
 def hello():
     return 'Hello HBNB'
-@app.route('/hbnb')
+@app.route('/hbnb',strict_slashes=False)
 def Hhello():
     return 'HBNB'
-@app.route('/c/<text>')
+@app.route('/c/<text>',strict_slashes=False)
 def displayC(text):
      return 'C {}'.format(text.replace('_', ' '))
-@app.route('/python')
-@app.route('/python/<text>')
+@app.route('/python',strict_slashes=False)
+@app.route('/python/<text>',strict_slashes=False)
 def displayPy(text="is cool"):
     return 'Python {}'.format(text.replace('_', ' '))
 
