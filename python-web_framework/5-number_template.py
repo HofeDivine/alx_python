8cf9d46b4775d45872480a66a9aab13280f9f737
HofeDivine <hofedive@gmail.com>
2024-02-10 17:32:47 -1200

    commit

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -1,39 +1,47 @@
-
-"""
- a new script that starts a Flask web application:
-
-Your web application must be listening on 0.0.0.0, port 5000
-
-"""
+import logging
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Configure logging
+logging.basicConfig(filename='app.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s: %(message)s')
+
 @app.route('/', strict_slashes=False)
 def hello():
+    app.logger.info('Accessed / route')
     return 'Hello HBNB!'
 
 @app.route('/hbnb', strict_slashes=False)
 def Hhello():
+    app.logger.info('Accessed /hbnb route')
     return 'HBNB'
 
 @app.route('/c/<text>', strict_slashes=False)
 def displayC(text):
+    app.logger.info('Accessed /c/{}'.format(text))
     return 'C {}'.format(text.replace('_', ' '))
-@app.route('/python', strict_slashes=False)
+
 @app.route('/python/<text>', strict_slashes=False)
 def displayPy(text="is cool"):
+    app.logger.info('Accessed /python/{}'.format(text))
     return 'Python {}'.format(text.replace('_', ' '))
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def displayNumber(n):
-    return '{} is a number'.format(n)
+    app.logger.info('Accessed /number/{}'.format(n))
+    if isinstance(n, int):
+        return '{} is a number'.format(n)
+    else:
+        return 'Not a valid integer'
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def displayNumberTemplate(n):
+    app.logger.info('Accessed /number_template/{}'.format(n))
     if isinstance(n, int):
         return render_template('5-number.html', n=n)
-    
+    else:
+        return 'Not a valid integer'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
+
