0c6c3b83e0d13f52186fe96be4aac3a4ea2b7446
HofeDivine <hofedive@gmail.com>
2024-02-10 17:34:24 -1200

    commit

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -1,3 +1,9 @@
+"""
+ a new script that starts a Flask web application:
+
+Your web application must be listening on 0.0.0.0, port 5000
+
+"""
 import logging
 from flask import Flask, render_template
 
