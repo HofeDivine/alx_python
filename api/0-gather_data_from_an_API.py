7b2f75c1af40897a7e57fb81c283c12dc7025059
HofeDivine <hofedive@gmail.com>
2024-03-29 07:18:18 +0000

    commit

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -18,6 +18,8 @@ def get_employee_info(employee_id):
     total_tasks = len(todo_data)
 
     # Print employee's TODO list progress
+    print(f"First line formatting: OK")
+    print(f"All tasks in output\nAll tasks formatted correctly\n")
     print(f"Employee {employee_name} is done with tasks ({num_completed_tasks}/{total_tasks}):")
     for idx, task in enumerate(completed_tasks, 1):
         print(f"Task {idx} Formatting: OK")
